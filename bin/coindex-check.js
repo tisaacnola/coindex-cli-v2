4bd6c295b40a2c0f07475b6b9e50d49850deaf62
tisaacnola <oisaac47@gmail.com>
2024-11-01 13:34:29 -0500

    competed check price command

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -5,8 +5,12 @@ import check from "../commands/check.js";
 program
   .command("price")
   .description("Check price of coins")
-  .option("--coin <type>", "Add specifc coins in CSV format", "BTC,ETH,USDT")
-  .option("--curr <currency>", "Change the currency", "USD")
+  .option(
+    "--coin <type>",
+    "Add specifc coins in CSV format",
+    "bitcoin,ethereum,tether"
+  )
+  .option("--curr <currency>", "Change the currency", "usd")
   .action((cmd) => check.price(cmd));
 
 program.parse(process.argv);
--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,17 @@
+import KeyManager from "../lib/KeyManager.js";
+import CryptoAPI from "../lib/CryptoAPI.js";
+
 const check = {
-  price(cmd) {
-    console.log(cmd.coin, cmd.curr);
+  async price(cmd) {
+    try {
+      const keyManager = new KeyManager();
+      const key = keyManager.getKey();
+      const api = new CryptoAPI(key);
+      const priceOutputData = await api.getPriceData(cmd.coin, cmd.curr);
+      console.log(priceOutputData);
+    } catch (error) {
+      console.error(error.message.red);
+    }
   },
 };
 
